6ec3f5337a9ea3e5ce6d08527aa7e4dd8671fed8
Grigor Danut <grigor@seamlesscare.ie>
2024-07-25 22:31:00 +0100

    Added the code to the MainActivity.java file

---
+++ app/src/main/java/com/example/smartgpclinic/LoginBy.java
@@ -0,0 +1,14 @@
+package com.example.smartgpclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginBy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_by);
+    }
+}
--- app/src/main/java/com/example/smartgpclinic/MainActivity.java
+++ app/src/main/java/com/example/smartgpclinic/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.smartgpclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Action button NFC
+        Button buttonNFC = findViewById(R.id.btnNfc);
+        buttonNFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nfc = new Intent(MainActivity.this, NFCActivity.class);
+                startActivity(nfc);
+            }
+        });
+
+        //Action button Log In
+        Button btn_LogInMain = findViewById(R.id.btnLogInMain);
+        btn_LogInMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginBy.class));
+            }
+        });
+
+        //Action button Register
+        Button btn_registerMain = findViewById(R.id.btnRegisterMain);
+        btn_registerMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sign = new Intent(MainActivity.this, CheckUniqueCode.class);
+                startActivity(sign);
+            }
+        });
     }
 }
